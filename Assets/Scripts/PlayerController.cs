97beab460fe1ebf120033bab6b2631355acac43e
FHBaroni <86305650+FHBaroni@users.noreply.github.com>
2023-04-13 17:12:23 -0300

    Lesson 6.2 - Research and Troubleshooting

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float speed = 30.0f;
-    [SerializeField] private float turnSpeed = 20;
+    [SerializeField] private float speed;
+    [SerializeField] private float rpm;
+    [SerializeField] private float horsePower;
+    [SerializeField] GameObject centerOfMass;
+    [SerializeField] TextMeshProUGUI speedometerText;
+    [SerializeField] TextMeshProUGUI rpmText;
+    [SerializeField] List<WheelCollider> allWheels;
+    [SerializeField] int wheelsOnGround;
+
+    private float turnSpeed = 20;
     private float horizontaInput;
     private float forwardInput;
 
@@ -13,12 +22,17 @@ public class PlayerController : MonoBehaviour
     public Camera hoodCamera;
     public KeyCode switchKey;
 
+    private Rigidbody playerRb;
+
     public string inputID;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        playerRb = GetComponent<Rigidbody>();
         hoodCamera.enabled = false;
+        playerRb.centerOfMass = centerOfMass.transform.localPosition;
     }
 
     // Update is called once per frame
@@ -26,28 +40,44 @@ public class PlayerController : MonoBehaviour
     {
         horizontaInput = Input.GetAxis("Horizontal" + inputID);
         forwardInput = Input.GetAxis("Vertical" + inputID);
-        //move vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * forwardInput * speed);
-        transform.Rotate(Vector3.up, turnSpeed * horizontaInput * Time.deltaTime);
-        //transform.Translate(Vector3.right* Time.deltaTime * turnSpeed);
+
+        if (IsOnGround())
+        {
+            //move vehicle forward
+            playerRb.AddRelativeForce(Vector3.forward * forwardInput * horsePower * (playerRb.velocity.magnitude < 0.1f ? 10f : 1f));
+            //transform.Translate(Vector3.forward * Time.deltaTime * forwardInput * speed);
+            transform.Rotate(Vector3.up, turnSpeed * horizontaInput * Time.deltaTime);
+            //transform.Translate(Vector3.right* Time.deltaTime * turnSpeed);
+            speed = Mathf.RoundToInt(playerRb.velocity.magnitude * 3.6f);
+            speedometerText.SetText("Speed:" + speed + "Km/h");
+
+            rpm = (speed % 30) * 35;
+            rpmText.SetText("RPM: " + rpm);
+        }
 
         if (Input.GetKeyDown(switchKey))
-        {
-        
+        {        
             mainCamera.enabled = !mainCamera.enabled;
             hoodCamera.enabled = !hoodCamera.enabled;
-
-            /* if (active)
-             {
-                 mainCamera.enabled = true;//!mainCamera.enabled;
-                 hoodCamera.enabled = false; // !hoodCamera.enabled;
-             }
-             else
-             {
-                 mainCamera.enabled = false;//!mainCamera.enabled;
-                 hoodCamera.enabled = true; // !hoodCamera.enabled;
-             }
-            */
+        }
+    }
+    bool IsOnGround()
+    {
+        wheelsOnGround = 0;
+        foreach (WheelCollider wheel in allWheels)
+        {
+            if (wheel.isGrounded)
+            {
+                wheelsOnGround++;
+            }
+        }
+        if (wheelsOnGround == 4)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 }
